448161e326e87c089148df555c233446214d0e99
Amar Civic <60967723+Ashty1337@users.noreply.github.com>
2022-01-19 21:41:19 +0100

    Added Animations to Player

--- PlayerMovement.gd
+++ PlayerMovement.gd
@@ -8,6 +8,7 @@ const MAXSPEED = 80
 const JUMPFORCE = 300
 
 var motion = Vector2()
+var facing_right = true
 func _ready():
 	pass # Replace with function body.
 
@@ -17,17 +18,32 @@ func _physics_process(delta):
 	if motion.y > MAXFALLSPEED:
 		motion.y = MAXFALLSPEED
 	
+	if facing_right == true:
+		$Sprite.scale.x = 1
+	else:
+		$Sprite.scale.x = -1
+	
 	motion.x = clamp(motion.x,-MAXSPEED,MAXSPEED)
 	
 	if Input.is_action_pressed("right"):
 		motion.x += ACCEL
+		facing_right = true
+		$AnimationPlayer.play("PlayerRunAni")
 	elif Input.is_action_pressed("left"):
 		motion.x += -ACCEL
+		facing_right = false
+		$AnimationPlayer.play("PlayerRunAni")
 	else:
 		motion.x = lerp(motion.x,0,0.2)
+		$AnimationPlayer.play("PlayerIdleAni")
 	
 	if is_on_floor():
 		if Input.is_action_just_pressed("jump"):
+			$AnimationPlayer.play("Jump")
 			motion.y = -JUMPFORCE
-		
+	else:
+		if motion.y < 0:
+			$AnimationPlayer.play("Jump")
+		elif motion.y > 0:
+			$AnimationPlayer.play("PlayerFallAni")
 	motion = move_and_slide(motion,UP)
